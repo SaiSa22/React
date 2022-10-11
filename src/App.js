38dd9726a934ec1d5af9d9735d4b247cb49b432a
SaiSa22 <ssabti@gmail.com>
2022-10-11 15:32:47 -0500

    Version0.1

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const toggleReminder = (id) => {
 
   return (
     <div className='container'>
-      <Header onAdd={() => setShowAddTask(!showAddTask)} title='Task Tracker' showAddTask={showAddTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} title='Tasks Tracker' showAddTask={showAddTask} />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length >0 ? <Tasks deleteTask={deleteTask} onToggle={toggleReminder} tasks={tasks} />:'All Done'}
     </div>
